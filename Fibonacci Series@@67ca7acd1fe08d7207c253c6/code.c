d54467958433f79ec68f19beaa4ca5db04dd729b
B7-Tech <bhumika0203.becse24@chitkara.edu.in>
2025-03-14 13:17:56 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,8 +1,8 @@
 fibonacci(){
     for (int i = 1; i<=n; i++){
         int c = int a + int b;
-        a = b;
-        b = c;
+        int a = int b;
+        int b = int c;
     }
     return 0;
 }
