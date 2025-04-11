c29739a57e242a394bf8aa20698eebb8d46ff0d8
B7-Tech <bhumika0203.becse24@chitkara.edu.in>
2025-04-11 15:44:29 +0530

    Solution to Find the Most Frequent Character

--- Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
+++ Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
@@ -1,30 +1,54 @@
+// #include <stdio.h>
+// #include <string.h>
+
+// char maxOccurringChar(char str[]) {
+//     int count[256] = {0};
+//     int maxfreq = 0;
+//     char ans = '\0';
+
+//     for (int i = 0; str[i] != '\0'; i++) {
+//         unsigned char ch = str[i];
+//         count[ch]++;
+
+//         if (count[ch] > maxfreq) {
+//             maxfreq = count[ch];
+//             ans = ch;
+//         } else if (count[ch] == maxfreq && ch < ans) {
+//             ans = ch; 
+//         }
+//     }
+//     return ans;
+// }
+
+// int main() {
+//     char str[100];
+//     fgets(str, sizeof(str), stdin);
+//     char result = maxOccurringChar(str);
+//     printf("%c", result);
+
+//     return 0;
+// }
+
+
 #include <stdio.h>
 #include <string.h>
 
-char maxOccurringChar(char str[]) {
-    int count[256] = {0};
-    int maxfreq = 0;
-    char ans = '\0';
-
-    for (int i = 0; str[i] != '\0'; i++) {
-        unsigned char ch = str[i];
-        count[ch]++;
-
-        if (count[ch] > maxfreq) {
-            maxfreq = count[ch];
-            ans = ch;
-        } else if (count[ch] == maxfreq && ch < ans) {
-            ans = ch; 
+char mostFrequent(char *s) {
+    int count[256] = {0}, max = 0;
+    char result = 0;
+    
+    for (int i = 0; s[i]; i++) count[s[i]]++;
+    for (int i = 0; i < 256; i++)
+        if (count[i] > max || (count[i] == max && i < result)) {
+            max = count[i];
+            result = i;
         }
-    }
-    return ans;
+    return result;
 }
 
 int main() {
-    char str[100];
-    fgets(str, sizeof(str), stdin);
-    char result = maxOccurringChar(str);
-    printf("%c", result);
-
+    char s[100];
+    fgets(s, 100, stdin);
+    printf("%c", mostFrequent(s));
     return 0;
 }
